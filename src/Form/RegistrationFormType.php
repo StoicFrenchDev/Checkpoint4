2485f58e79c76d045a7b16f13be987c1b6ad6da6
Benjamin RICHARD <richard.benjamin69@yahoo.fr>
2023-07-19 17:46:55 +0200

    must fix login

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,11 +6,13 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class RegistrationFormType extends AbstractType
 {
@@ -23,6 +25,26 @@ class RegistrationFormType extends AbstractType
                 'constraints' => [
                     new IsTrue([
                         'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'constraints' => [
+                    new Assert\NotBlank(['message' => 'Prénom obligatoire']),
+                    new Assert\Length([
+                        'max' => 100,
+                        'maxMessage' => 'Prénom trop long',
+                    ]),
+                ],
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'constraints' => [
+                    new Assert\NotBlank(['message' => 'Nom obligatoire']),
+                    new Assert\Length([
+                        'max' => 100,
+                        'maxMessage' => 'Nom trop long',
                     ]),
                 ],
             ])
